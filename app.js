33da675d047e8875e8a084ffd3fc5691a0f4a6d0
rachel <rachel-g@naver.com>
2019-07-19 02:41:38 +0900

    2.4 Brush Size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 // canvas element 는 두개의 사이즈를 가지고 와야한다. css와 현재 화면에 그려있는!
 canvas.width = 700;
@@ -10,6 +12,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 // 페인팅 스톱!
 function stoppainting() {
@@ -47,6 +50,22 @@ function handleColorClick(event) {
     ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+    // console.log(event.target.value);
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+    if(filling == true) {
+        filling = false;
+        mode.innerText = "Fill"
+    } else {
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
+
 // canvas가 존재하는지 확인 & canvas 안 마우스 움직임 감지
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
@@ -59,3 +78,11 @@ if(canvas) {
 Array.from(colors).forEach(color => 
     color.addEventListener("click", handleColorClick)
 );
+
+if(range) {
+    range.addEventListener("input", handleRangeChange)
+}
+
+if(mode) {
+    mode.addEventListener("click", handleModeClick)
+}
