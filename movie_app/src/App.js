cb2272dc0ca7b9013580620c982eae8f146e8bd1
hyung <hj.gwon@specupad.com>
2019-08-02 11:24:35 +0900

    3-1. Lifecycle Events on React

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -5,24 +5,38 @@ import Movie from './Movie'
 const movies = [
   {
     title: "The Lion King",
-    poster: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjAo6T0wtvjAhVTPnAKHaIwAUUQjRx6BAgBEAU&url=http%3A%2F%2Fimg.movist.com%2F%3Fimg%3D%2Fx00%2F05%2F11%2F79_p1.jpg&psig=AOvVaw1W2bOeglkIY0jqX9AJNWUC&ust=1564537607634427",
+    poster: "http://img.movist.com/?img=/x00/05/11/79_p1.jpg",
   },
   {
     title: "Aladdin",
-    poster: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj_wPn_wtvjAhWKE4gKHfakCOwQjRx6BAgBEAU&url=http%3A%2F%2Fimg.movist.com%2F%3Fimg%3D%2Fx00%2F05%2F11%2F49_p1.jpg&psig=AOvVaw0cWj3Xu6pVWkWdq2x7iBMV&ust=1564537631928647",
+    poster: "http://img.movist.com/?img=/x00/05/11/49_p1.jpg",
   },
   {
     title: "The King's Letters",
-    poster: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiN4ZWSw9vjAhXFFIgKHeqQDogQjRx6BAgBEAU&url=http%3A%2F%2Fimg.movist.com%2F%3Fimg%3D%2Fx00%2F05%2F03%2F76_p1.jpg&psig=AOvVaw1S-9_01l8kR9C0RELgdzEi&ust=1564537670319653",
+    poster: "http://img.movist.com/?img=/x00/05/03/76_p1.jpg",
   },
   {
     title: "Spider-Man: Far From Home",
-    poster: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiW3-uIw9vjAhXOE4gKHZnuB_gQjRx6BAgBEAU&url=http%3A%2F%2Fimg.movist.com%2F%3Fimg%3D%2Fx00%2F04%2F90%2F19_p1.jpg&psig=AOvVaw1w0qXLFU-PSI8SpRmWsF8v&ust=1564537650575421",
+    poster: "http://img.movist.com/?img=/x00/05/13/00_p1.jpg",
   }
 ]
 
 class App extends Component {
-  render(){
+
+  // Render: ComponentWillMount() -> render() -> ComponentDidMounr()
+  // Update ComponentWillReceiveProps() -> shouldComponentUpdate() -> ComponentWillUpdate(): spinner(로딩중) -> reder() -> ComponentDidUpdate(): 로딩중 메세지나 아이콘 숨기기
+  // 컴포넌트가 존재하기 시작하면 리액트 세계는 will mount -> did render -> did mount를 작동한다
+
+  // componentWillMount(){ // api 요청
+  //   console.log('will mount')
+  // }
+
+  // componentDidMount(){
+  //   console.log('did mount')
+  // }
+
+  render(){ // 데이터
+    // console.log('did render')
     return (
       <div className="App">
         {movies.map((movie, index) => {
