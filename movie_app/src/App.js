00ddd4fab1a97bd6f2cb1a26abf8dd897624f790
hyung <rachel-g@naver.com>
2019-08-27 12:28:40 +0900

    Async Await

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -8,23 +8,32 @@ class App extends Component {
   // Update ComponentWillReceiveProps() -> shouldComponentUpdate() -> ComponentWillUpdate(): spinner(로딩중) -> reder() -> ComponentDidUpdate(): 로딩중 메세지나 아이콘 숨기기
   // 컴포넌트가 존재하기 시작하면 리액트 세계는 will mount -> did render -> did mount를 작동한다
 
-  state = {
-
-  }
+  state = {}
 
   // componentDidMount: 컴포넌트가 mount되면 5초 기다리고 greeting을 업데이트 함
   componentDidMount(){
-    fetch('https://yts.lt/api/v2/list_movies.json?sort_by=like_count')
-    .then(potato => potato.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=like_count')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render(){ // 데이터
